439f953a3d0b39b9b7839d1d60c596fd055699de
Quentin Young <youngqp3@gmail.com>
2022-01-04 12:51:55 -0800

    labs 9 and 10 complete'

---
+++ collecting_user_input_lab_10.py
@@ -0,0 +1,21 @@
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+# below is a function containing our code
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_input = input("Please enter an IPv4 IP address: ")
+    
+    # display the input back to the user.
+    print("You told me the IPv4 address is: " + user_input)
+
+    #get vendor name from user
+    vendor = input("Print vendor name of your device: ")
+
+    #print device vendor
+    print("Your device is a ", vendor)
+
+    
+# this calls main
+main()
---
+++ print_lab_09.py
@@ -0,0 +1,14 @@
+# Lab 09 on printing in python
+
+print(42)
+print("192.168.30.5", "A1:02:CC:04", "255.255.255.0")
+
+
+a=3.14
+print("a=", a)
+print("a = \n", a)
+print("a", "b")
+print("a","b", sep="")
+print("a","b", sep=":)")
+print("I am thinking", end="... ... ... \n")
+print("I am thinking", end="!!!")
