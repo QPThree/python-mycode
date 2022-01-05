609e962cdeea5e84c7127a4423dafbdcef52cd89
Quentin Young <youngqp3@gmail.com>
2022-01-05 12:14:08 -0800

    finished lab 23

---
+++ if_conditionals_lab_23.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def main():
+    # create the string hostname
+    hostname = "MTG"
+    # test logic with the `if` statement
+    # what to do if this statement is found to be true
+    if hostname == "MTG":
+        print("The hostname was found to be MTG")
+
+
+     #!/usr/bin/env python3
+    hostname = input("What value should we set for hostname?")
+    if hostname.lower()== "mtg":
+        print("The hostname was found to be MTG")
+        print("Hostname matches expected config")
+
+    print("Exiting the script")
+    
+
+if __name__ == "__main__":
+    main()
