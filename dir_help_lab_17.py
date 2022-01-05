104824febf8a75bcd9f58c6e9ad787a2af9e10bd
Quentin Young <youngqp3@gmail.com>
2022-01-05 08:26:25 -0800

    finisihed labed 17

---
+++ dir_help_lab_17.py
@@ -0,0 +1,20 @@
+#Lab 17 
+
+print(dir(str))
+
+x = "EthGIG01"
+x.lower()
+# help(x.lower)
+
+print(x.lower().startswith("eth"))
+x = open("myfile.txt", "w")
+
+dir(x)
+# help(x.read)
+
+x=5
+dir(x)
+
+x.__add__(6)
+
+print(x.__le__(3))
