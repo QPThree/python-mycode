0a3ff43709276cac9c1fb21ea8083ab491f5e092
Quentin Young <youngqp3@gmail.com>
2022-01-05 13:33:48 -0800

    added comments

--- custom_logic_script.py
+++ custom_logic_script.py
@@ -6,20 +6,20 @@ def main():
     if, elif, else - A simple program using conditionals to make a decision."""
 
 
-    errorMessage = "You need to select a valid option"
+    errorMessage = "You need to select a valid option" #Displayed if user input doesnt match choice
     choices = '\n 0. AMZN\n 1. TSLA\n 2. MSFT\n 3. GOOGL'
     data = [
-        {"company": "Amazon", "price": 3296.40, "p/e": 64.46 },
-        {"company": "Tesla", "price": 1090.90, "p/e": 354.00 },
-        {"company": "Microsoft", "price": 317.04, "p/e": 35.44 },
-        {"company": "Google", "price": 2755.23, "p/e": 26.54 }]
+        { "company": "Amazon", "price": 3296.40, "p/e": 64.46 },
+        { "company": "Tesla", "price": 1090.90, "p/e": 354.00 },
+        { "company": "Microsoft", "price": 317.04, "p/e": 35.44 },
+        { "company": "Google", "price": 2755.23, "p/e": 26.54 }]
 
     
     # wrap connection in a float() to accept decimals as numbers
     choice = float(input("Select stock to analyze" + choices))
 
     # must match indexes in data. Could be refactored to variable
-    # i.e. c = choice; printCompanyData(data[c])
+    # i.e. printCompanyData(data[choice])
     if choice == 0:
         printCompanyData(data[0])
     elif choice == 1:
