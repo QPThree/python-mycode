5c32f060f0dcad983aca381c5300b4bb2820ac69
Quentin Young <youngqp3@gmail.com>
2022-01-07 09:47:25 -0800

    fixed max

--- function_grab_bag.py
+++ function_grab_bag.py
@@ -8,7 +8,7 @@ def findMaxOfThreeInts(num1: int, num2:int, num3:int)-> int:
     if (num1 > num2):
         if (num1 > num3):
             max = num1
-        elif (num3> num1):
+        else :
             max = num3
     elif (num2 > num3):
         max = num2
@@ -38,7 +38,7 @@ def findEvenNumbersInList(list1:list) -> list:
 
 def main():
 
-    print(findMaxOfThreeInts(-5,-4,3))
+    print(findMaxOfThreeInts(0,3,3))
     print(sumAllNumbersInList([1,2,3,4,5]))
     print(multiplyAllNumbersInList([-2,-3, 5, -6]))
     print(findEvenNumbersInList([1,2,3,4,5,6,7,8,9,10]))
