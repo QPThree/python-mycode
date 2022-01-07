d8666b8e7cbfb96774acd6a762178b2a683a5f75
Quentin Young <youngqp3@gmail.com>
2022-01-07 09:12:29 -0800

    commented the functions

--- function_grab_bag.py
+++ function_grab_bag.py
@@ -16,16 +16,18 @@ def findMaxOfThreeInts(num1: int, num2:int, num3:int)-> int:
         max = num3
     return max
 
-
 #gets sum of all numbers in  a list
 def sumAllNumbersInList(list1: list) -> int:
     return sum(list1)
 
+#returns the product of every number in a list
 def multiplyAllNumbersInList(list1:list) -> int:
     product = 1 #multiply by 1 to start off
     for num in list1:
         product *= num
     return product
+
+#finds even numbers in a list and returns those numbers as a new list
 def findEvenNumbersInList(list1:list) -> list:
     evenList=[]
     for num in list1:
